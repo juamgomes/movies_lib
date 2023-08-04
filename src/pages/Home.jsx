f100ab19583d03e3c0d3eeb435a2c85c84f420a7
Juan Gomes <89032763+juamgomes@users.noreply.github.com>
2023-08-04 15:24:01 -0400

    Criei o componente movieCard que cotem a Imagem nome e nota do filme.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import {React, useState, useEffect} from 'react'
+import MovieCard from '../componets/MovieCard';
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-  const {topMovies, SetTopMovies} = useState([])
+  const [topMovies, SetTopMovies] = useState([])
 
   const getTopRateMovies = async (url) => {
 
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data)
+    SetTopMovies(data.results);
   };
 
 
@@ -24,8 +25,15 @@ const Home = () => {
   }, [])
 
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h2>Melhores filmes:</h2>
+      <div>
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+      </div>
+    </div>
+  );  
+  
+};
 
 export default Home
