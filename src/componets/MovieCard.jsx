4f0c46fe5666c51fe74d3f4fecf912555768c5e2
Juan Gomes <89032763+juamgomes@users.noreply.github.com>
2023-08-21 15:55:59 -0400

    Aumentei o tamanho dos cards

--- src/componets/MovieCard.jsx
+++ src/componets/MovieCard.jsx
@@ -7,7 +7,7 @@ const imageURL = import.meta.env.VITE_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
   return (
-    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 text-white mb-9 px-2'>
+    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-30 text-white mb-9 px-2'>
       <div className='flex flex-col justify-between rounded-lg bg-zinc-950 p-4 h-full'>
         
         {showLink && (
